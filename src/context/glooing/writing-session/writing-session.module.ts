366ae95f44a154dbd2270bcca821166f1e05a8e7
magiof <tjddnjs7559@gamil.com>
2024-04-09 23:08:48 +0900

    chore: 모듈임포트

--- src/context/glooing/writing-session/writing-session.module.ts
+++ src/context/glooing/writing-session/writing-session.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { WritingSessionService } from './writing-session.service';
 import { WritingSessionController } from './writing-session.controller';
+import { UserBadgeModule } from 'src/context/reward/userBadge/userBadge.module';
 
 @Module({
+  imports: [UserBadgeModule],
   controllers: [WritingSessionController],
   providers: [WritingSessionService],
   exports: [WritingSessionService],
