8d190dab2eb1f03d0002d8826cbc41c9efeda2e5
magiof <tjddnjs7559@gamil.com>
2024-05-31 23:05:49 +0900

    fix: 콘솔로깅제거

--- src/middlewares/InjectAccount.middleware.ts
+++ src/middlewares/InjectAccount.middleware.ts
@@ -17,14 +17,7 @@ export class InjectAccountMiddleware implements NestMiddleware {
     if (req.baseUrl.includes('refresh-token')) return next();
     if (req.baseUrl.includes('delete')) return next();
     const accessToken = req.headers.authorization?.split('Bearer ')[1];
-    console.log(
-      'url:',
-      req.url,
-      'accessToken',
-      accessToken,
-      '타입:',
-      typeof accessToken,
-    );
+
     if (!accessToken || accessToken === null || accessToken === 'null')
       return next();
     const { role, type } = jwt.decode(accessToken) as {
